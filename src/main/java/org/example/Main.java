b491fcb8f3528411892976e3f1e5102d99be5da1
Vigneshwar Balaji <vickycloud2810@gmail.com>
2024-08-25 17:38:29 +0530

    add core java employee management app

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import org.example.Model.Employee;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employeeOne = new Employee(1, "Adam", "HR", 5000, 25);
+        Employee employeeTwo = new Employee(2, "John", "Engineering", 1000, 21);
+        Employee employeeThree = new Employee(3, "Zoe", "Engineering", 3000, 23);
+        Employee employeeFour = new Employee(4, "Yash", "Marketing", 5000, 25);
+        Employee employeeFive = new Employee(5, "Beau", "Marketing", 3000, 27);
+        Employee employeeSix = new Employee(6, "Gilly", "Sales", 4000, 29);
+        Employee employeeSeven = new Employee(7, "Newton", "Sales", 1000, 30);
+        Employee employeeEight = new Employee(8, "Albert", "Sales", 1000, 45);
+        Employee employeeNine = new Employee(9, "Lusie", "HR", 2000, 39);
+        Employee employeeTen = new Employee(10, "Rachael", "HR", 2000, 22);
+
+        List<Employee>employeeList = Arrays.asList(employeeOne, employeeTwo, employeeThree, employeeFour, employeeFive,
+                                                employeeSix, employeeSeven, employeeEight, employeeNine, employeeTen);
+
+        employeeList.stream()
+                    .filter(employee -> employee.getSalary() > 3000 && employee.getDepartment() == "HR")
+                    .map(employee -> employee.getName())
+                    .collect(Collectors.toList())
+                    .forEach(System.out::println);
+
+        System.out.println("\n\nEmployees default sorting order:");
+//        Collections.sort(employeeList, Comparator.comparing(Employee::getAge));
+        employeeList.stream().forEach(System.out::println);
+
+        System.out.println("\n\nEmployees sorted by age:");
+        Collections.sort(employeeList, Comparator.comparing(Employee::getAge));
+        employeeList.stream().forEach(System.out::println);
+        System.out.println("\n\nEmployees sorted by name:");
+        Collections.sort(employeeList, Comparator.comparing(Employee::getName));
+        employeeList.stream().forEach(System.out::println);
+    }
+}
---
+++ src/main/java/org/example/Model/Employee.java
@@ -0,0 +1,87 @@
+package org.example.Model;
+
+import java.util.Objects;
+
+public class Employee {
+    private int id;
+    private String name;
+    private String department;
+    private int salary;
+    private int age;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String name, String department, int salary, int age) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && salary == employee.salary && age == employee.age && Objects.equals(name, employee.name) && Objects.equals(department, employee.department);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, department, salary, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+
+}
